d0407a8520a8847ee9c3f2894710da5c3c08fb70
john <Johnaellis0@gmail.com>
2017-07-15 17:39:39 -0700

    Initial commit

---
+++ tfreg.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import numpy as np
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+cd
+import tensorflow as tf
+sess = tf.InteractiveSession()
+
+#Bookeeping:
+model_path = "model/model.ckpt"
+
+#Building the map
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.global_variables_initializer())
+
+y = tf.matmul(x,W) + b
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+# Running the loop
+for _ in range(1000):
+  batch = mnist.train.next_batch(100)
+  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+
+# Checking the Data
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print("Regression Accuracy: %r"% accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+
+
+#CNN
+
+#Initialization functions
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+#Convolution Functions
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
+#1st Layer
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+#Layer 2 (64 features)
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+#Fully Connected Layer
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+#Dropout
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+#Readout Layer
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+#Evaluation
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+#Create the Saver
+#saver = tf.train.Saver({'wc1':W_conv1, 'bc1':b_conv1, 'wc2':W_conv2, 'bc2':b_conv2, 'wfc1':W_fc1, 'bfc1':b_fc1, 'wfc2':W_fc2, 'bfc2':b_fc2})
+#[W_conv1, b_conv1, W_conv2, b_conv2, W_fc1, b_fc1, W_fc2, b_fc2]
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  for i in range(2000):
+    batch = mnist.train.next_batch(50)
+    if (i+1) % 100 == 0:
+      train_accuracy = accuracy.eval(feed_dict={
+          x: batch[0], y_: batch[1], keep_prob: 1.0})
+      print('step %d, training accuracy %g' % (i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    if (i+1) % 5000 == 0:
+      saver.save(sess, 'tfcnn', global_step=i)
+
+  save_path = saver.save(sess, model_path)
+  print("Model saved in file: %s" % save_path)
+
+
+  for i in range(100):
+    batch = mnist.test.next_batch(50)
+    accs.append(accuracy.eval(feed_dict={
+      x: batch[0], y_: batch[1], keep_prob: 1.0}))
+  
+  print('test accuracy %g' % np.mean(accs))
+
+  #batch = mnist.test.next_batch(500)
+
+  #print('test accuracy %g' % accuracy.eval(feed_dict={
+      #x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+
+
